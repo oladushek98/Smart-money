819aaff1f5e81d9816173a217f99c93b6831236f
Alexander Sokolovskiy 650502 <ninjaduckyduck@gmail.com>
2019-04-12 20:50:34 +0300

    add string representation to models

--- main/models.py
+++ main/models.py
@@ -7,6 +7,10 @@ CURRENCY_LENGTH = 3
 
 
 class FinancialNode(models.Model):
+
+    def __str__(self):
+        return f'\'{self.name}\''
+
     icon = models.CharField(max_length=ICON_LENGTH, null=True, blank=True)
     name = models.CharField(max_length=NAME_LENGTH)
     currency = models.CharField(max_length=CURRENCY_LENGTH, null=True,
@@ -16,11 +20,18 @@ class FinancialNode(models.Model):
 
 # доходы
 class Income(FinancialNode):
+    def __str__(self):
+        return f'Доходы : {super()}'
+
     monthly_plan = models.IntegerField(null=True, blank=True)
 
 
 # счета
 class Account(FinancialNode):
+    def __str__(self):
+        return f'Счет {"долговой" if self.is_debt_account else ""} : ' \
+            f'{super()}'
+
     amount = models.IntegerField(null=True, blank=True)
     is_debt_account = models.BooleanField()
     take_into_balance = models.BooleanField()
@@ -28,16 +39,26 @@ class Account(FinancialNode):
 
 # расходы
 class Cost(FinancialNode):
+    def __str__(self):
+        return f'Расходы : {super()}'
+
     monthly_plan = models.IntegerField(null=True, blank=True)
 
 
 # цель
 class Goal(FinancialNode):
+    def __str__(self):
+        return f'Цели : {super()}'
+
     plan = models.IntegerField(null=True, blank=True)
 
 
 # транзакция
 class Transaction(models.Model):
+    def __str__(self):
+        return f'Из {self.transaction_from} в ' \
+            f'{self.transaction_to} дата : {self.data_from}'
+
     transaction_from = models.ForeignKey(FinancialNode,
                                          related_name='source',
                                          on_delete=models.CASCADE)
