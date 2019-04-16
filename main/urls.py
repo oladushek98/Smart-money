fdbdf69cd8085e2b4f9bff9348726e4892ee724a
Alexander Sokolovskiy 650502 <ninjaduckyduck@gmail.com>
2019-04-16 12:21:59 +0300

    add income create api endpoint

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 
+from main.views.api import incomeApi
 from main.views import index, authentication, user
 
 urlpatterns = [
     path('', index.IndexView.as_view(), name='main'),
-    path('auth/register/', authentication.RegisterView.as_view(), name='register'),
+    path('auth/register/', authentication.RegisterView.as_view(),
+         name='register'),
     path('auth/login/', authentication.LoginView.as_view(), name='login'),
     path('auth/logout/', authentication.LogoutView.as_view(), name='logout'),
     path('user/<int:id>/', index.UserpageView.as_view(), name='userpage'),
-    path('user/<int:pk>/edit', user.UserUpdateView.as_view(), name='edit_user')
+    path('user/<int:pk>/edit', user.UserUpdateView.as_view(), name='edit_user'),
+    path('api/create_income/<int:pk>', incomeApi.IncomeCreate.as_view(),
+         name='create_income'),
 ]
