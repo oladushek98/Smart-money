43573de9313b1ee2ec8eef742b253cd772bcdc25
KupiyanovVladislav <9412252@mail.ru>
2019-04-24 20:03:26 +0300

    convert currency api

--- main/views/api/convertApi.py
+++ main/views/api/convertApi.py
@@ -7,9 +7,9 @@ from main.utils import convert_currency
 
 class Converter(View):
 
-    def get(self, request, **kwargs):
+    def put(self, request, **kwargs):
 
-        body = kwargs
+        body = json.loads(request.body)
         body['result'] = convert_currency(body['amount'], body['main'], body['secondary'])
 
         return JsonResponse(body)
