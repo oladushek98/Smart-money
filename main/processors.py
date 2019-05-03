08201429b79aa4d0016b2f7d75d0440df16a7dcc
Alexander Sokolovskiy 650502 <ninjaduckyduck@gmail.com>
2019-05-03 19:10:23 +0300

    fix trubles with diferent currency

--- main/processors.py
+++ main/processors.py
@@ -2,47 +2,82 @@ from django.db.models import Sum, Q
 
 from main.forms import IncomeForm, TransactionCreateForm, AccountForm, CostForm
 from main.models import Income, Transaction, Account, Cost
+from main.utils import convert_currency
 
 ITEM_AMOUNT = 0
+
+
+def calculate_cost_amount(cost):
+    amount = 0
+    for transaction in cost.destination.filter(delete=False).all():
+        if cost.currency == transaction.choice_currency:
+            amount += transaction.amount
+        else:
+            amount += convert_currency(transaction.amount,
+                                       transaction.choice_currency,
+                                       cost.currency)
+    return amount
+
+
+def calculate_income_amount(income):
+    amount = 0
+    for transaction in income.source.filter(delete=False).all():
+        if income.currency == transaction.choice_currency:
+            amount += transaction.amount
+        else:
+            amount += convert_currency(transaction.amount,
+                                       transaction.choice_currency,
+                                       income.currency)
+    return amount
+
+
+def calculate_account_amount(account):
+    amount = 0
+    for transaction in account.destination.filter(delete=False).all():
+        if account.currency == transaction.choice_currency:
+            amount += transaction.amount
+        else:
+            amount += convert_currency(transaction.amount,
+                                       transaction.choice_currency,
+                                       account.currency)
+
+    for transaction in account.source.filter(delete=False).all():
+        if account.currency == transaction.choice_currency:
+            amount -= transaction.amount
+        else:
+            amount -= convert_currency(transaction.amount,
+                                       transaction.choice_currency,
+                                       account.currency)
+
+    return amount
 
 
 def processor(request):
     incoms = list(Income.objects.filter(delete=False,
                                         user_id=request.user.id).all())
     for incom in incoms:
-        incom.amount = sum(
-            map(lambda x: x.amount, incom.source.filter(delete=False).all()))
-
-    income_create_from = IncomeForm()
-    account_create_form = AccountForm()
-    cost_create_form = CostForm()
-    add_transaction = TransactionCreateForm()
+        incom.amount = calculate_income_amount(incom)
 
     accounts = list(Account.objects.filter(delete=False,
                                            user_id=request.user.id).all())
-
     for account in accounts:
-        amount_plus = sum(map(lambda x: x.amount,
-                              account.destination.filter(delete=False).all()))
-        amount_minus = sum(
-            map(lambda x: x.amount, account.source.filter(delete=False).all()))
-
-        account.amount += amount_plus - amount_minus
+        account.amount = calculate_account_amount(account)
 
     costs = list(Cost.objects.filter(delete=False,
                                      user_id=request.user.id).all())
-
     for cost in costs:
-        amount_plus = sum(map(lambda x: x.amount,
-                              cost.destination.filter(delete=False).all()))
-
-        cost.amount = amount_plus if amount_plus is not None else 0
+        cost.amount = calculate_cost_amount(cost)
 
     transactions = list(
         Transaction.objects.filter(delete=False,
                                    user_id=request.user.id).all()[:9])
     for transaction in transactions:
         transaction.data_from = transaction.data_from.__str__()
+
+    income_create_from = IncomeForm()
+    account_create_form = AccountForm()
+    cost_create_form = CostForm()
+    add_transaction = TransactionCreateForm()
 
     return {'INCOMES': incoms,
             'ACCOUNTS': accounts,
--- main/views/transaction.py
+++ main/views/transaction.py
@@ -4,7 +4,7 @@ from django.db.models import Q, Sum
 from django.shortcuts import render
 
 from main.forms import TransactionUpdateForm
-from main.models import Transaction, Account
+from main.models import Transaction, Account, Cost
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
@@ -14,20 +14,29 @@ class TransactionUpdateView(FormView):
     template_name = 'transaction/update_transaction.html'
 
     def get_chart_data(self, transaction):
-        destination = Account.objects.filter(id=transaction.transaction_to) \
+        destination = Account.objects.filter(id=transaction.transaction_to.id) \
             .first()
 
-        amount = 0 if destination is None else destination.amount
-        amount += Transaction.objects \
+        amount = 0
+        if destination is None:
+            destination = Cost.objects.filter(id=transaction.transaction_to.id) \
+                .first()
+        else:
+            amount = destination.amount
+        calculate = Transaction.objects \
             .filter(id__lt=transaction.id,
                     delete=False,
-                    user=self.request.user) \
+                    user=self.request.user.id) \
             .aggregate(
-            value=Sum('amount', filter=(Q(transaction_to=destination.id))) \
-                  - Sum('amount', filter=(Q(transaction_from=destination.id))))
+                plus=Sum('amount', filter=(Q(transaction_to=destination.id))),
+                minus=Sum('amount', filter=(Q(transaction_from=destination.id)))
+            )
+        amount += calculate['plus'] if calculate['plus'] is not None else 0
+        amount -= calculate['minus'] if calculate[
+                                            'minus'] is not None else 0
 
         return [['транзакция', transaction.amount],
-                [destination.name.replace('"', '\''), amount['value']]]
+                [destination.name.replace('"', '\''), amount]]
 
     def get(self, request, *args, **kwargs):
         transaction_id = int(request.path.split('/')[-1])
